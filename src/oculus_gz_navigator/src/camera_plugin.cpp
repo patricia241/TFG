def8ad223ed66bfcb0a17ef2d6ab0d7ebb8d0ef3
Patricia Colmenar <p.colmenar.2019@alumnos.urjc.es>
2023-02-26 19:18:28 +0100

    Mejorado Camera Plugin ahora se mueve en función de la orientacion de los ejes de la camara

--- src/oculus_gz_navigator/src/camera_plugin.cpp
+++ src/oculus_gz_navigator/src/camera_plugin.cpp
@@ -23,7 +23,7 @@ namespace oculus_gz_navigator
             std::cout << "-----------------------------------------" << std::endl;
             std::cout << std::endl;
 
-            link_ = _model->GetLink("link");
+            link_ = _model->GetLink("base_link");
 
             // ROS node
             node_ = gazebo_ros::Node::Get(_sdf);
@@ -42,61 +42,62 @@ namespace oculus_gz_navigator
         // ROS subscriptor
         rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr pose_sub_;
 
+        void multiply_vector_matrix(ignition::math::Vector4<float> point, ignition::math::Matrix4<float> tr_mat, ignition::math::Vector4<float>& result) {
+          for(int i = 0;  i < 4; i++){
+            for (int j = 0; j < 4; j++){
+              result[i] += tr_mat(i, j) * point[j];
+            }
+          }
+        }
+
+        /*
+          @local_pose: variable que indica los metros que se quiere mover la cámara en X, Y, Z tomando como sistema de referencias la cámara
+          @global_pose: es dónde se guardará el resultado, las coordenadas respecto al sistema de referencias del mundo dónde habrá que situar la cámara
+        */
         void get_transformed_coordinate(ignition::math::Vector4<float> local_pose, ignition::math::Vector4<float>& global_pose) {
+          ignition::math::Vector4<float> traslation_vector;
           ignition::math::Pose3d pose = link_->WorldPose();
-          float roll = -pose.Rot().Roll();
-          float pitch = -pose.Rot().Pitch();
-          float yaw = -pose.Rot().Yaw();
+          float roll = pose.Rot().Roll();
+          float pitch = pose.Rot().Pitch();
+          float yaw = pose.Rot().Yaw();
 
-          std::cout << "Angles: " << roll << " " << pitch << " " << yaw << std::endl;
-
-          ignition::math::Matrix4<float> traslation_mat(1, 0, 0, -pose.Pos().X(),
-                                                 0, 1, 0, -pose.Pos().Y(),
-                                                 0, 0, 1, 0,
-                                                 0, 0, 0, 1);
           ignition::math::Matrix4<float> rotation_x_mat(1, 0, 0, 0,
-                                                 0, cos(roll), -sin(roll), 0,
-                                                 0, sin(roll), cos(roll), 0,
-                                                 0, 0, 0, 1);
+                                                        0, cos(roll), -sin(roll), 0,
+                                                        0, sin(roll), cos(roll), 0,
+                                                        0, 0, 0, 1);
           ignition::math::Matrix4<float> rotation_y_mat(cos(pitch), 0, -sin(pitch), 0,
-                                                 0, 1, 0, 0,
-                                                 sin(pitch), 0, cos(pitch), 0,
-                                                 0, 0, 0, 1);
+                                                        0, 1, 0, 0,
+                                                        sin(pitch), 0, cos(pitch), 0,
+                                                        0, 0, 0, 1);
           ignition::math::Matrix4<float> rotation_z_mat(cos(yaw), -sin(yaw), 0, 0,
-                                                 sin(yaw), cos(yaw), 0, 0,
-                                                 0, 0, 1, 0,
-                                                 0, 0, 0, 1);
-
-          ignition::math::Matrix4<float> traslation_rot_mat = traslation_mat * rotation_x_mat * rotation_y_mat * rotation_z_mat;
-          global_pose = local_pose * traslation_rot_mat;
+                                                        sin(yaw), cos(yaw), 0, 0,
+                                                        0, 0, 1, 0,
+                                                        0, 0, 0, 1);
+          // Roto local_pose según los ángulos que esté rotada mi cámara
+          ignition::math::Matrix4<float> rot_mat = rotation_x_mat * rotation_y_mat * rotation_z_mat;
+          multiply_vector_matrix(local_pose, rot_mat, traslation_vector);
+          
+          // Traslado la cámara
+          ignition::math::Vector4<float> current_pose(pose.Pos().X(), pose.Pos().Y(), pose.Pos().Z(), 1);
+          global_pose = current_pose + traslation_vector;          
         }
 
         // If in msg Position I get (0, 0, 1) increment z position 1m. Rotation set directly with msg info. 
         void camera_position_callback(const geometry_msgs::msg::Pose::SharedPtr msg) {
           ignition::math::Vector3d speeds(0, 0, 0); 
           ignition::math::Pose3d pose = link_->WorldPose();
-          /*
-          float k = 5.0;
-          pose.SetX(pose.Pos().X() + msg->position.x / k);
-          pose.SetY(pose.Pos().Y() + msg->position.y / k);
-          pose.SetZ(pose.Pos().Z());
-          */
           ignition::math::Vector4<float> global_pose;
-          ignition::math::Vector4<float> local_pose(msg->position.x/5, msg->position.y/5, 0, 1);
+          ignition::math::Vector4<float> local_pose(msg->position.x, msg->position.y, msg->position.z, 1);
 
-          if (msg->position.x != 0 && msg->position.y != 0) {
-            get_transformed_coordinate(local_pose, global_pose);
-
-            pose.SetX(global_pose.X() + pose.Pos().X());
-            pose.SetY(global_pose.Y() + pose.Pos().Y());
-            pose.SetZ(pose.Pos().Z() + pose.Pos().Z());
-            
-            std::cout << "Pose X: " << global_pose.X() << " Pose Y: " << global_pose.Y() << std::endl;
-
-            link_->SetWorldPose(pose);
-          }
+          get_transformed_coordinate(local_pose, global_pose);
           
+          pose.SetX(global_pose.X());
+          pose.SetY(global_pose.Y());
+          pose.SetZ(global_pose.Z());
+          
+          link_->SetWorldPose(pose);
           link_->SetLinearVel(speeds);
+          link_->SetAngularVel(speeds); 
         }
   };
 
