d1e33df45f520720e96d18aa844b0527bb5b4f3a
Patricia Colmenar <p.colmenar.2019@alumnos.urjc.es>
2023-02-28 20:52:06 +0100

    Arreglado camera plugin

--- src/oculus_gz_navigator/src/camera_plugin.cpp
+++ src/oculus_gz_navigator/src/camera_plugin.cpp
@@ -42,59 +42,70 @@ namespace oculus_gz_navigator
         // ROS subscriptor
         rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr pose_sub_;
 
-        void multiply_vector_matrix(ignition::math::Vector4<float> point, ignition::math::Matrix4<float> tr_mat, ignition::math::Vector4<float>& result) {
-          for(int i = 0;  i < 4; i++){
-            for (int j = 0; j < 4; j++){
-              result[i] += tr_mat(i, j) * point[j];
-            }
-          }
+        ignition::math::Matrix3<double> generate_matrix3_from_point(double x, double y, double z) {
+          ignition::math::Matrix3<double> point_mat(0, 0, x, 
+                                                    0, 0, y,
+                                                    0, 0, z);
+          return point_mat;
         }
 
-        /*
-          @local_pose: variable que indica los metros que se quiere mover la cámara en X, Y, Z tomando como sistema de referencias la cámara
-          @global_pose: es dónde se guardará el resultado, las coordenadas respecto al sistema de referencias del mundo dónde habrá que situar la cámara
-        */
-        void get_transformed_coordinate(ignition::math::Vector4<float> local_pose, ignition::math::Vector4<float>& global_pose) {
-          ignition::math::Vector4<float> traslation_vector;
-          ignition::math::Pose3d pose = link_->WorldPose();
-          float roll = pose.Rot().Roll();
-          float pitch = pose.Rot().Pitch();
-          float yaw = pose.Rot().Yaw();
-
-          ignition::math::Matrix4<float> rotation_x_mat(1, 0, 0, 0,
-                                                        0, cos(roll), -sin(roll), 0,
-                                                        0, sin(roll), cos(roll), 0,
-                                                        0, 0, 0, 1);
-          ignition::math::Matrix4<float> rotation_y_mat(cos(pitch), 0, -sin(pitch), 0,
-                                                        0, 1, 0, 0,
-                                                        sin(pitch), 0, cos(pitch), 0,
-                                                        0, 0, 0, 1);
-          ignition::math::Matrix4<float> rotation_z_mat(cos(yaw), -sin(yaw), 0, 0,
-                                                        sin(yaw), cos(yaw), 0, 0,
-                                                        0, 0, 1, 0,
-                                                        0, 0, 0, 1);
-          // Roto local_pose según los ángulos que esté rotada mi cámara
-          ignition::math::Matrix4<float> rot_mat = rotation_x_mat * rotation_y_mat * rotation_z_mat;
-          multiply_vector_matrix(local_pose, rot_mat, traslation_vector);
-          
-          // Traslado la cámara
-          ignition::math::Vector4<float> current_pose(pose.Pos().X(), pose.Pos().Y(), pose.Pos().Z(), 1);
-          global_pose = current_pose + traslation_vector;          
+        ignition::math::Matrix3<double> get_rotation_mat(double roll, double pitch, double yaw) {
+          ignition::math::Matrix3<double> rotx_mat(1, 0, 0, 
+                                                   0, cos(roll), -sin(roll),
+                                                   0, sin(roll), cos(roll));
+          ignition::math::Matrix3<double> roty_mat(cos(pitch), 0, sin(pitch), 
+                                                   0, 1, 0,
+                                                   -sin(pitch), 0, cos(pitch));
+          ignition::math::Matrix3<double> rotz_mat(cos(yaw), -sin(yaw), 0, 
+                                                   sin(yaw), cos(yaw), 0,
+                                                   0, 0, 1);                                        
+          return rotz_mat * roty_mat * rotx_mat;
         }
 
+        ignition::math::Matrix3<double> generate_change_base_matrix() {
+          ignition::math::Pose3d current_pose = link_->WorldPose();
+          ignition::math::Matrix3<double> axis_x_end = generate_matrix3_from_point(1, 0, 0);
+          ignition::math::Matrix3<double> axis_y_end = generate_matrix3_from_point(0, 1, 0);
+          ignition::math::Matrix3<double> axis_z_end = generate_matrix3_from_point(0, 0, 1);
+          ignition::math::Matrix3<double> position = generate_matrix3_from_point(current_pose.X(), current_pose.Y(), current_pose.Z());
+          ignition::math::Matrix3<double> rot_mat = get_rotation_mat(current_pose.Roll(), current_pose.Pitch(), current_pose.Yaw());
+
+          // DEBUG
+          std::cout << "MATRIZ ROTACIÓN: " << std::endl;
+          std::cout << rot_mat(0, 0) << " " <<  rot_mat(0, 1) << " " << rot_mat(0, 2) << std::endl;
+          std::cout << rot_mat(1, 0) << " " <<  rot_mat(1, 1) << " " << rot_mat(1, 2) << std::endl;
+          std::cout << rot_mat(2, 0) << " " <<  rot_mat(2, 1) << " " << rot_mat(2, 2) << std::endl;
+
+          // Rotated and translated axis
+          ignition::math::Matrix3<double> rt_axis_x_end = rot_mat * axis_x_end + position;
+          ignition::math::Matrix3<double> rt_axis_y_end = rot_mat * axis_y_end + position;
+          ignition::math::Matrix3<double> rt_axis_z_end = rot_mat * axis_z_end + position;
+
+          ignition::math::Matrix3<double> change_base_mat(rt_axis_x_end(0, 2), rt_axis_y_end(0, 2), rt_axis_z_end(0, 2),
+                                                          rt_axis_x_end(1, 2), rt_axis_y_end(1, 2), rt_axis_z_end(1, 2),
+                                                          rt_axis_x_end(2, 2), rt_axis_y_end(2, 2), rt_axis_z_end(2, 2));
+
+
+          // DEBUG
+          std::cout << "MATRIZ CAMBIO DE BASE: " << std::endl;
+          std::cout << change_base_mat(0, 0) << " " <<  change_base_mat(0, 1) << " " << change_base_mat(0, 2) << std::endl;
+          std::cout << change_base_mat(1, 0) << " " <<  change_base_mat(1, 1) << " " << change_base_mat(1, 2) << std::endl;
+          std::cout << change_base_mat(2, 0) << " " <<  change_base_mat(2, 1) << " " << change_base_mat(2, 2) << std::endl;
+
+          return change_base_mat;
+        }
         // If in msg Position I get (0, 0, 1) increment z position 1m. Rotation set directly with msg info. 
         void camera_position_callback(const geometry_msgs::msg::Pose::SharedPtr msg) {
-          ignition::math::Vector3d speeds(0, 0, 0); 
+          ignition::math::Vector3d speeds(0, 0, 0);
           ignition::math::Pose3d pose = link_->WorldPose();
-          ignition::math::Vector4<float> global_pose;
-          ignition::math::Vector4<float> local_pose(msg->position.x, msg->position.y, msg->position.z, 1);
 
-          get_transformed_coordinate(local_pose, global_pose);
+          ignition::math::Matrix3 point_global = generate_change_base_matrix() * generate_matrix3_from_point(msg->position.x, msg->position.y, msg->position.z);
+          ignition::math::Vector3d final_result(point_global(0, 2), point_global(1, 2), point_global(2, 2));
+          // ignition::math::Quaternion orientation(msg->orientation.x, msg->orientation.y, msg->orientation.z);
           
-          pose.SetX(global_pose.X());
-          pose.SetY(global_pose.Y());
-          pose.SetZ(global_pose.Z());
-          
+          std::cout << "X: " << final_result.X() << " Y: " << final_result.Y() << " Z: " << final_result.Z() << std::endl;
+          pose.Set(final_result, pose.Rot());
+
           link_->SetWorldPose(pose);
           link_->SetLinearVel(speeds);
           link_->SetAngularVel(speeds); 
